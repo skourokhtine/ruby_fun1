b1d20a1abf014846d20ff140baf6865f9bc598a1
Serguei Kourokhtine <skourokhtine@gmail.com>
2015-06-09 16:21:48 -0400

    Exercise2 Completed. See comments in code...

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+meal = 55
+puts "The good meal cost #{meal}"
+
+tip = 55 * 0.20
+puts "I left a twenty percent tip, which is #{tip}"
+
+puts "how much was your total bill"
+puts "My total bill was #{meal + tip}"
+
+#The code does not prompt the user because I wanted to mix strings
+#math together.
